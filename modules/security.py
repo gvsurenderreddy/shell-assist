3904862a436b3c81d980b0ca26c3f9f47828e651
linostar <linux.anas@gmail.com>
2016-01-31 11:39:30 +0200

    Added public/private key functions

--- modules/security.py
+++ modules/security.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
@@ -7,5 +9,46 @@ from Crypto.Hash import SHA256
 
 class Security:
 	"""To encrypt and/or sign communications between clients across a server"""
-	pass
 
+	KEY_PREFIX = "assist_"
+
+	def __init__(self, username = "me", keypath = "./"):
+		self.username = str(username)
+		self.keypath = keypath
+		self.privkey = None
+		self.pubkey = None
+		self.receiver_keys = dict()
+	
+	def create_key_pair(self):
+		self.privkey = RSA.generate(4096, Random.new().read)
+		with open(self.keypath + self.KEY_PREFIX + self.username + "_priv", "w") as fpriv:
+			fpriv.write(self.privkey.exportKey())
+		with open(self.keypath + self.KEY_PREFIX + self.username + ".pub", "w") as fpub:
+			fpub.write(self.privkey.publickey().exportKey())
+
+	def load_my_privkey(self):
+		keyfilename = self.keypath + self.KEY_PREFIX + self.username + "_priv"
+		if os.path.exists(keyfilename):
+			try:
+				self.privkey = RSA.importKey(open(keyfilename, "r").read())
+			except:
+				self.privkey = None
+				print("Error: invalid private key.")
+
+	def load_my_pubkey(self):
+		keyfilename = self.keypath + self.KEY_PREFIX + self.username + ".pub"
+		if os.path.exists(keyfilename):
+			try:
+				self.pubkey = RSA.importKey(open(keyfilename, "r").read())
+			except:
+				self.pubkey = None
+				print("Error: invalid public key.")
+
+	def load_other_pubkey(self, other = "anon"):
+		keyfilename = self.keypath + self.KEY_PREFIX + other + ".pub"
+		if os.path.exists(keyfilename):
+			try:
+				self.receiver_keys[other] = RSA.importKey(open(keyfilename, "r").read())
+			except:
+				self.receiver_keys[other] = None
+				print("Error: invalid public key.")
