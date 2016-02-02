2bc58858036fb2477ca872b61d6d34dab20d2b46
linostar <linux.anas@gmail.com>
2016-02-02 13:26:49 +0200

    more client/server connection code

--- modules/connection.py
+++ modules/connection.py
@@ -1,36 +1,92 @@
 #!/usr/bin/env python
 
+import sys
 import socket
-from socketserver import TCPServer, BaseRequestHandler
+import select
 
 
-class CustomTCPHandler(BaseRequestHandler):
-	"""Overrides BaseRequestHandler class with a custom handler method"""
-	
-	def handle(self):
-		self.server.recv_data = self.request.recv(1024).strip()
+class ForwardServer:
+	"""Forward-proxy server"""
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+		self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	def start_server(self):
+		try:
+			self.forward.connect((self.host, self.port))
+			return self.forward
+		except Exception as e:
+			print(e)
 
 
 class ServerConnection:
-	"""For server (forward-proxy) connection and transmission of data"""
+	"""For server connection and transmission of data"""
+
+	BUFFER_SIZE = 1024
+	DELAY = 0.001
+	MAX_CONNECTIONS = 100
+	input_list = []
+	channels = {}
 	
 	def __init__(self, host = "localhost", port = 9876):
 		self.host = host
 		self.port = port
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.server.bind((host, port))
+		self.server.listen(self.MAX_CONNECTIONS)
+
+	def listening_loop(self):
+		self.input_list.append(self.server)
+		while True:
+			ss = select.select
+			readlist, writelist, exceptlist = ss(self.input_list, [], [], self.DELAY)
+			for ready_server in readlist:
+				if ready_server == self.server:
+					self.on_accept()
+					break
+				self.data = ready_server.recv(self.BUFFER_SIZE)
+				if len(self.data):
+					self.on_recv(ready_server)
+				else:
+					self.on_close(ready_server)
+					break
+
+	def on_accept(self):
+		forward_sock = ForwardServer(self.host, self.port).start_server()
+		client_sock, client_addr = self.server.accept()
+		if forward_sock:
+			print("{} has connected.".format(client_addr))
+			self.input_list.append(client_sock)
+			self.input_list.append(forward_sock)
+			self.channels[client_sock] = forward_sock
+			self.channels[forward_sock] = client_sock
+		else:
+			print("Could not establish connection with server.")
+			print("Closing connection with client {}.".format(client_addr))
+			client_sock.close()
+
+
+	def on_recv(self, dest):
+		self.channels[dest].send(self.data)
+
+	def on_close(self, dest):
+		print("{} has disconnected.".format(dest.getpeername()))
+		self.input_list.remove(dest)
+		self.input_list.remove(self.channels[dest])
+		dest.close()
+		self.channels[dest].close()
+		del self.channels[self.channels[out]]
+		del self.channels[dest]
 
 	def start_server(self):
 		try:
-			self.server = TCPServer((self.host, self.port), CustomTCPHandler)
-			self.server.serve_forever()
+			self.listening_loop()
 		except KeyboardInterrupt:
 			print("\nStopping server...")
-
-	def stop_server(self):
-		try:
-			self.server.shutdown()
-			self.server.server_close()
-		except:
-			print("\nCould not stop server. Perhaps it has already stopped.")
+			self.server.close()
+			sys.exit(0)
 
 
 class ClientConnection:
@@ -44,5 +100,7 @@ class ClientConnection:
 	def connect_to_server(self):
 		try:
 			self.sock.connect((self.host, self.port))
-		finally:
+		except KeyboardInterrupt:
+			print("\nStopping client...")
 			self.sock.close()
+			sys.exit(0)
